c06d1e6ca75014da56580cb88f56eb8fe6c69996
danielwu <daniel.wu.14@gmail.com>
2017-09-24 19:29:10 -0700

    Starting a small game of minesweeper
    current errors: get adjacent not working, not counting up space values
    correctly

---
+++ minesweeper.py
@@ -0,0 +1,68 @@
+import random
+class MineBoard:
+    def __init__(self, board_x = 16, board_y = 16, mine_count = 40):
+        self.x = board_x
+        self.y = board_y
+        self.board = []
+        self.mines = mine_count
+
+    def init_board(self):
+        for i in range(self.x):
+            board_row = []
+            for j in range(self.y):
+                board_row.append(0)
+            self.board.append(board_row)
+        return
+    
+    def seed_board(self):
+        for i in range(self.mines):
+            rand_x = int(random.random() * self.x)
+            rand_y = int(random.random() * self.y)
+            while self.board[rand_x][rand_y] == 'x':
+                rand_x = int(random.random() * self.x)
+                rand_y = int(random.random() * self.y)
+
+            self.board[rand_x][rand_y] = 'x'
+        return
+
+    def get_adjacent(self,p_x,p_y):
+        valid_x = [-1,0,1]
+        valid_y = [-1,0,1]
+        if p_x == 0:
+            valid_x = [0, 1]
+        if p_y == 0:
+            valid_y = [0, 1]
+        if p_x == self.x-1:
+            valid_x = [-1, 0]
+        if p_y == self.y-1:
+            valid_y = [-1, 0]
+
+        return valid_x, valid_y
+
+    def load_numbers(self):
+        for i in range(self.x):
+            for j in range(self.y):
+                if self.board[i][j] == 'x':
+                    x_range, y_range = self.get_adjacent(i,j)
+                    for a in range(3):
+                        for b in range(3):
+                            if a == 0 and b == 0:
+                                if not self.board[i+x_range[a]][j+y_range[b]] == 'x':
+                                    self.board[i+x_range[a]][j+y_range[b]] += 1
+
+
+    def to_string(self):
+        for i in self.board:
+            for j in i:
+                print(j, end= '|')
+            print('\n')
+game_board = MineBoard(16,16,40)
+
+print(game_board.board)
+game_board.init_board()
+game_board.seed_board()
+game_board.load_numbers()
+
+print(game_board.to_string())
+
+
